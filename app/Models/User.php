bb283a89878db795fc37d8c176b4e904a73963ac
Paul-Ben <paulben.ajene@gmail.com>
2025-09-26 15:55:24 +0100

    feat(user): add role management using spatie permissions
    Add HasRoles trait and userRole field to enable role-based permissions
    Implement methods to sync and retrieve primary role

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'userRole',
     ];
 
     /**
@@ -42,4 +44,23 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Sync the userRole column with the user's primary role
+     */
+    public function syncUserRole(): void
+    {
+        $primaryRole = $this->roles->first();
+        if ($primaryRole) {
+            $this->update(['userRole' => $primaryRole->name]);
+        }
+    }
+
+    /**
+     * Get the user's primary role name
+     */
+    public function getPrimaryRole(): ?string
+    {
+        return $this->userRole ?? $this->roles->first()?->name;
+    }
 }
