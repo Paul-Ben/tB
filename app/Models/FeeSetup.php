05f6c2393347d010a0b18cf9c766e80211a89040
Paul-Ben <paulben.ajene@gmail.com>
2025-09-28 14:33:50 +0100

    feat(models): add new Eloquent models for school management system
    Add core models including Term, Result, Manager, Student, Subject, FeeSetup, Guardian, Classroom, Transaction, Registration, ClassCategory, ManualPayment, and SchoolSession with basic functionality and SchoolSession with additional methods for status management

---
+++ app/Models/FeeSetup.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FeeSetup extends Model
+{
+    use HasFactory;
+}
