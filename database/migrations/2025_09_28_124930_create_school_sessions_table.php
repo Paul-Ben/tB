f35d9c0c158baf84017a861e7017a4f88032e149
Paul-Ben <paulben.ajene@gmail.com>
2025-09-28 14:32:58 +0100

    feat(database): add school management system database migrations
    create tables for class categories, school sessions, subjects, classrooms, terms, fee setups, guardians, managers, registrations, transactions, students, manual payments, and results

---
+++ database/migrations/2025_09_28_124930_create_school_sessions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('school_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->string('sessionName');
+            $table->enum('status', ['active','inactive'])->default('inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('school_sessions');
+    }
+};
